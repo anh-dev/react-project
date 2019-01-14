f11339a652d3126a39d310d8538cbc0757e3f7d3
Andrii Holub <andr.holub@gmail.com>
2019-01-14 15:38:00 +0200

    refactoring structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,10 +4,6 @@ import classes from './Person.css';
 
 const person = (props) => {
 
-    const rand = Math.random();
-    if (rand > 0.7) {
-        throw new Error('Something went wrong');
-    }
 
     return (
         <div className={classes.Person}>
